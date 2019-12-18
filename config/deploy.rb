103de678c60b39995567906a34a8ba02c2c14fda
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-12-18 14:28:39 +0900

    デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,9 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
-# Capistranoのログの表示に利用する
 set :application, 'quuta'
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:ono2342/quuta.git'
+# set :repo_url,  'git@github.com:ono2342/quuta.git'
+set :repo_url,  'https://github.com/ono2342/quuta.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
