fd0c24a249665e99eb22c314ce3fb93f79e55f8f
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-12-21 17:18:17 +0900

    画像プレビュー表示

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,12 +1,24 @@
-window.onload = function() {
-  new Vue({
-    el: '#editor',
-    data: {
-    input: '',
-    },
-    filters: {
-    marked: marked,
-    },
+$(document).on("turbolinks:load", function() {
+  var old = ""
+  var v = ""
+
+  function Load() {
+    if(document.URL.match("articles/new")) {
+      var el = $("#article_text")[0];
+      replaceMarkdown(el);
+
+    }
+  }
+  function replaceMarkdown(elm) {
+    var v = elm.value;
+    if (old != (v = elm.value)) {
+      old = v;
+      str = $("#article_text").val();
+      $("#marked-area").html(marked(str));
+    }
+  }
+  $("#article_text").on("keyup",function () {
+    replaceMarkdown(this);
   });
   
   $(function(){
@@ -18,4 +30,5 @@ window.onload = function() {
       extraHeaders: {"X-CSRF-Token": $("meta[name=csrf-token]").attr("content")},
     });
   });
-};
+  setInterval(Load, 1000);
+});
