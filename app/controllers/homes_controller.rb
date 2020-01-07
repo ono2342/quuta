981965a18268faae70dcbf63db4dcd6d6598c74c
ono2342 <56573437+ono2342@users.noreply.github.com>
2020-01-07 13:32:50 +0900

    create top signup

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @user = User.new
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :check_captcha, only: [:create]
   prepend_before_action :customize_sign_up_params, only: [:create]
 
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      sign_in @user unless user_signed_in?
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def customize_sign_up_params
@@ -16,5 +26,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     unless verify_recaptcha(model: resource)
       respond_with_navigational(resource) { render :new }
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password)
   end
 end
