67216f97da6597c55223be34e557b2049a0c37bd
takao8989 <56573708+takao8989@users.noreply.github.com>
2020-01-08 19:35:22 +0900

    update-schema

--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_01_06_060433) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
     t.string "email", default: "", null: false
+    t.string "icon"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
