493495d2ef666d9c3684cb3d49fdeb5e511445f5
takao8989 <56573708+takao8989@users.noreply.github.com>
2020-01-08 15:46:07 +0900

    お問い合わせフォーム作成2

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,6 +3,7 @@ class Inquiry
 
   attr_accessor :name, :email, :message
   
-  validates :name, :presence => {:message => '名前を入力してください'}
-  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  VALID_EMAIL_REGIX    = /\A[^@\s]+@[^@\s]+\z/
+  validates :name,       presence: { message: "名前を入力してください" }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGIX,   message: "フォーマットが不適切です"}
 end
