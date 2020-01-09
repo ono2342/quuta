8c073a3b02a741e2ed8550baf3e8492c7828b6b6
takao8989 <56573708+takao8989@users.noreply.github.com>
2020-01-09 17:15:19 +0900

    update-development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,12 +34,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {host: 'http://18.177.164.39/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:                 587,
     address:              'smtp.gmail.com',
-    domain:               'gmail.com',
+    domain:               'smtp.gmail.com',
     user_name:            ENV['EMAIL_ADDRESS'],
     password:             ENV['EMAIL_PASSWORD'],
     authentication:       'login',
